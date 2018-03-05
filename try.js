ef8853026f54354749debbbce2f58f5d6d58daa6
shamim ali <y13uc258@lnmiit.ac.in>
2018-03-05 22:01:07 +0530

    Update try.js

--- try.js
+++ try.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+  res.render('pages/index')
 })
 
 app.listen(port, (err) => {
