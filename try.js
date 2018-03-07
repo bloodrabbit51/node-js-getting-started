8c0b1e8bddde09a2df2ad24283cde7568c6f5a27
shamim ali <y13uc258@lnmiit.ac.in>
2018-03-07 18:26:21 +0530

    added web pages

--- try.js
+++ try.js
@@ -2,14 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+const username = 'drexter51'
+const authkey = '930994931286b111c09386b924cec9ac'
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=pune,in&units=metric&APPID='+authkey
+
+function data(name,id){
+	const msg = 'my name is ${name} with id as ${id}.'
+	console.log(msg)
+}
+
 app.get('/', (request, response) => {
-  response.send('Hello hi!')
+  data('shamim ali',134720)
+  response.send('hello ')
 })
 
 app.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('error', err)
   }
 
   console.log(`server is listening on ${port}`)
 })
+
+
